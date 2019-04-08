0b4a47981ac7028d1dad93085581302a31bf250d
Thor Anderson <ta.anderson@gmail.com>
2019-04-08 11:35:10 -0600

    Changing up PokeCard

---
+++ src/components/PokeCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card flat tile>
+                <v-img
+                  :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
+                  height="200px"
+                ></v-img>
+              </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
